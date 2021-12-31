e93eb1e217222147d55d986194d3797f8a12b822
sansrien <63850220+sansrien@users.noreply.github.com>
2021-12-31 11:49:07 +0800

    some edit

--- app.js
+++ app.js
@@ -22,17 +22,47 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
     res.render('home')
 })
 
 //new campground
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'my Backyard', description: " this is my description"})
-    await camp.save();
-    res.send(camp)
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({ title: 'my Backyard', description: " this is my description"})
+//     await camp.save();
+//     res.send(camp)
+// })
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
 })
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+}
+)
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    console.log(campground)
+    res.redirect(`campgrounds/${campground._id}`)
+})
+
+
+app.get('/campgrounds/:id', async (req, res) => {  //order matters! anything may be treated as :id if placed at first
+    const campground = await Campground.findById(req.params.id);
+    console.log(campground)
+    res.render('campgrounds/show', { campground })
+})
+
+app.get('/compgrounds/:id/edit', async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground});
+})
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
