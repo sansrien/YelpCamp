b17ad648dd6c33a6396183918936ae67344ad330
sansrien <63850220+sansrien@users.noreply.github.com>
2022-02-21 09:31:58 +0800

    added Reviews

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; // shortcut for mongoose.schema
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review' //reference is the review model
+        }
+    ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({ //but will have to loop through all reviews?
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 module.exports = mongoose.model('Campground', CampgroundSchema )  //export to the actual js
