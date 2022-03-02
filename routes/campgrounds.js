3133af4f88486330216d442ba0fbb1a5fcdb6e89
sansrien <63850220+sansrien@users.noreply.github.com>
2022-03-03 00:05:16 +0800

    restructuring routes using router.route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -10,26 +10,27 @@ const campgrounds = require('../controllers/campgrounds')
 
 
 //Show all campgrounds
-router.get('/', catchAsync(campgrounds.index))
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    // Post the new campground (Why not include the /new? -> bec form's action is directed to /campgrounds) 
+    //the functions validateCampground & catchAsync are executed to catch errors
+    .post(validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground))
 
 //Show new campground page
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-// Post the new campground (Why not include the /new? -> bec form's action is directed to /campgrounds) 
-//the functions validateCampground & catchAsync are executed to catch errors
-router.post('/', validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground))
+
 
 //show specific campground
-router.get('/:id', catchAsync(campgrounds.showCampground))
+router.route('/:id')
+    .get( catchAsync(campgrounds.showCampground))
+    //routerly edits on a specific campgrounds
+    .put( isLoggedIn, isAuthor, validateCampground,  catchAsync(campgrounds.updateCampground))
+    //delete a specific campground
+    .delete(isLoggedIn, isAuthor,catchAsync(campgrounds.deleteCampground))
 
 //show edit page for specific campground
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-//routerly edits on a specific campgrounds
-router.put('/:id', isLoggedIn, isAuthor, validateCampground,  catchAsync(campgrounds.updateCampground))
-
-//delete a specific campground
-router.delete('/:id', isLoggedIn, isAuthor,catchAsync(campgrounds.deleteCampground))
 
 
 module.exports= router;
